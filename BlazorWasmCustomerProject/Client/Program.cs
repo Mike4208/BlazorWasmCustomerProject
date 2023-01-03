de5242e51a01c68370862f023ea0b35b2277b240
Μιχάλης Μαρτασίδης <mixalis_4208@outlook.com>
2023-01-03 15:20:55 +0200

    Bing Autosuggest Map API

--- BlazorWasmCustomerProject/Client/Program.cs
+++ BlazorWasmCustomerProject/Client/Program.cs
@@ -1,13 +1,22 @@
 using BlazorWasmCustomerProject.Client;
+using BlazorWasmCustomerProject.Client.Services;
 using BlazorWasmCustomerProject.Client.Services.CustomerService;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<ICustomerService, CustomerService>();
+        builder.Services.AddScoped<GeocodingService>();
 
-await builder.Build().RunAsync();
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        builder.Services.AddScoped<ICustomerService, CustomerService>();
+
+        await builder.Build().RunAsync();
+    }
+}
